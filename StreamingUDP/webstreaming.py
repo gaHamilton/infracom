83049d9e712ecc7133722ab6c950dc5a37473179
Gabriel Hamilton <g.hamilton@uniandes.edu.co>
2020-04-07 18:50:02 -0500

    cambio de streaming de camara a un video guardado

--- StreamingUDP/webstreaming.py
+++ StreamingUDP/webstreaming.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask import render_template
 import threading
 import argparse
-import datetime
 import imutils
 import time
 import cv2
@@ -16,9 +15,13 @@ outputFrame = None
 lock = threading.Lock()
 # initialize a flask object
 app = Flask(__name__)
+
 # initialize the video stream and allow the camera sensor to
 # warmup
-vs = VideoStream(src=0).start()
+# TODO Elegir vs
+# vs = VideoStream(src=0).start()
+# vs2 = cv2.VideoCapture(0)
+vs3 = cv2.VideoCapture('../Doc/Prueba4.mp4')
 time.sleep(2.0)
 
 
@@ -34,23 +37,27 @@ def detect_motion():
     global vs, outputFrame, lock
 
     # loop over frames from the video stream
+    frame_counter = 0
     while True:
         # read the next frame from the video stream, resize it,
         # convert the frame to grayscale, and blur it
-        frame = vs.read()
-        frame = imutils.resize(frame, width=800)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        gray = cv2.GaussianBlur(gray, (7, 7), 0)
-        # grab the current timestamp and draw it on the frame
-        timestamp = datetime.datetime.now()
-        cv2.putText(frame, timestamp.strftime(
-            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+        # TODO elegir vs, aunque en verdad solo sirve para vs3
+        # frame = vs.read()
+        # ret, frame = vs2.read()
+        ret, frame = vs3.read()
 
+        if not ret:
+            frame_counter =0
+            vs3.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            ret, frame = vs3.read()
+        frame_counter += 1
+
+        frame = imutils.resize(frame, width=800)
         # acquire the lock, set the output frame, and release the
         # lock
         with lock:
             outputFrame = frame.copy()
+
 
 
 def generate():
@@ -94,7 +101,7 @@ if __name__ == '__main__':
     #                 help="# of frames used to construct the background model")
     # args = vars(ap.parse_args())
 
-    # TODO assign ip and port, above is to receive it as parameters when running the command
+    # assign ip and port, above is to receive it as parameters when running the command
     ip = "0.0.0.0"
     port = 8000
 
@@ -108,4 +115,7 @@ if __name__ == '__main__':
     # Without -----------> host=ip, port=port,
     app.run(host=ip, port=port, debug=True, threaded=True, use_reloader=False)
 # release the video stream pointer
-vs.stop()
+# TODO Elegir vs
+# vs.stop()
+# vs2.release()
+vs3.release()
