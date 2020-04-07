67fd6af59cde85e54a0f1e8903400a871a0b39e7
Gabriel Hamilton <g.hamilton@uniandes.edu.co>
2020-04-07 14:45:57 -0500

    correccion Streaming

--- StreamingUDP/webstreaming.py
+++ StreamingUDP/webstreaming.py
@@ -18,7 +18,6 @@ lock = threading.Lock()
 app = Flask(__name__)
 # initialize the video stream and allow the camera sensor to
 # warmup
-# vs = VideoStream(usePiCamera=1).start()
 vs = VideoStream(src=0).start()
 time.sleep(2.0)
 
@@ -27,6 +26,31 @@ time.sleep(2.0)
 def index():
     # return the rendered template
     return render_template("index.html")
+
+
+def detect_motion():
+    # grab global references to the video stream, output frame, and
+    # lock variables
+    global vs, outputFrame, lock
+
+    # loop over frames from the video stream
+    while True:
+        # read the next frame from the video stream, resize it,
+        # convert the frame to grayscale, and blur it
+        frame = vs.read()
+        frame = imutils.resize(frame, width=800)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+        # grab the current timestamp and draw it on the frame
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
+            "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+
+        # acquire the lock, set the output frame, and release the
+        # lock
+        with lock:
+            outputFrame = frame.copy()
 
 
 def generate():
@@ -60,22 +84,28 @@ def video_feed():
 
 # check to see if this is the main thread of execution
 if __name__ == '__main__':
-    # construct the argument parser and parse command line arguments
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ip", type=str, required=True,
-                    help="ip address of the device")
-    ap.add_argument("-o", "--port", type=int, required=True,
-                    help="ephemeral port number of the server (1024 to 65535)")
-    ap.add_argument("-f", "--frame-count", type=int, default=32,
-                    help="# of frames used to construct the background model")
-    args = vars(ap.parse_args())
+    # # construct the argument parser and parse command line arguments
+    # ap = argparse.ArgumentParser()
+    # ap.add_argument("-i", "--ip", type=str, required=True,
+    #                 help="ip address of the device")
+    # ap.add_argument("-o", "--port", type=int, required=True,
+    #                 help="ephemeral port number of the server (1024 to 65535)")
+    # ap.add_argument("-f", "--frame-count", type=int, default=32,
+    #                 help="# of frames used to construct the background model")
+    # args = vars(ap.parse_args())
+
+    # TODO assign ip and port, above is to receive it as parameters when running the command
+    ip = "0.0.0.0"
+    port = 8000
+
     # start a thread that will perform motion detection
-    t = threading.Thread(target=detect_motion, args=(
-        args["frame_count"],))
+    t = threading.Thread(target=detect_motion, args=())
     t.daemon = True
     t.start()
     # start the flask app
-    app.run(host=args["ip"], port=args["port"], debug=True,
-            threaded=True, use_reloader=False)
+
+    # With command ------> host=args["ip"], port=args["port"],
+    # Without -----------> host=ip, port=port,
+    app.run(host=ip, port=port, debug=True, threaded=True, use_reloader=False)
 # release the video stream pointer
 vs.stop()
