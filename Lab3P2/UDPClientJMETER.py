f574ef9496674725a9c42e4dbb97b1016b75087c
Gabriel Hamilton <g.hamilton@uniandes.edu.co>
2020-04-08 16:42:28 -0500

    cambios servidor UDP para JMeter

---
+++ Lab3P2/UDPClientJMETER.py
@@ -0,0 +1,53 @@
+import socket
+import threading
+import hashlib
+import time
+
+BUFF = 1024
+lock = threading.Lock()
+
+
+def cliente(num, last, lock):
+    # TCP ------> socket.AF_INET, socket.SOCK_STREAM
+    # UDP ------> socket.AF_INET, socket.SOCK_DGRAM
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    host = "127.0.0.1"
+    HostPort = (host, 20001)
+    i = 0
+
+    s.sendto("READY".encode(), HostPort)
+    hashR = ""
+    sha1 = hashlib.sha1()
+    fileName = "Recibido/received_file" + str(num)+".pdf"
+    with open(fileName, 'wb') as f:
+       print("Recibiendo archivo", num)
+       while True:
+            # print('receiving data...',i)
+            i += 1
+            data = s.recvfrom(BUFF)
+            if not data[0]:
+                break
+
+            elif (data[0].__contains__(b"FINM")):
+                val = data[0].find(b"FINM")
+                sha1.update(data[0][:val])
+                hashR = data[0][val:]
+                finT = time.time()
+                break
+            else:
+                sha1.update(data[0])
+                f.write(data[0])
+    f.close()
+
+    hashR = hashR[4:].decode()
+    print("HASH RECIBIDO: ", hashR)
+
+    # s.close()
+
+
+cantidadCliente = 25
+lock = threading.Lock()
+
+for i in range(cantidadCliente):
+    t = threading.Thread(target=cliente, args=(i, False, lock))
+    t.start()
--- Lab3P2/UDPServerJMETER.py
+++ Lab3P2/UDPServerJMETER.py
@@ -34,100 +34,53 @@ fileT = tup[1]
 numClientesC = 0
 atender = False
 
-
-def createLog():
-    print("Creando log")
-
-    # Fecha y hora --creacion log
-    fecha = datetime.datetime.now()
-
-    logName = "Logs/UDPLog" + str(fecha.timestamp()) + ".txt"
-    logFile = open(logName, "a")
-    logFile.write("Fecha: " + str(fecha) + "\n")
-
-    # Nombre del archivo y tamanio
-    fileN = fileName.split("/")
-    fileN = fileN[1]
-
-    logFile.write("Nombre del archivo: " + fileN + "\n")
-
-    fSize = os.path.getsize(fileName)
-
-    logFile.write("Tamanio del archivo: " + str(fSize) + " bytes\n")
-    logFile.write("----------------------------------------\n")
-
-    logFile.close()
-    return logName
-
-
-# Crear el archivo de log
-logName = createLog()
-
-
-def logDatosCliente(recepcion, tiempo, numPaqEnv, numPaqRecv, hashR, hash):
-    with lock:
-        paquetesE = "Numero de paquetes enviados por el servidor:" + str(numPaqEnv) + "\n"
-        paquetesR = "Numero de paquetes recibidos por el cliente:" + str(numPaqRecv) + "\n"
-        tiempoT = "Tiempo: " + str(tiempo) + " segundos\n"
-        separador = "\n---------------------------------------\n"
-        hash = "\nHASH calculado en el servidor: \n" + hash
-        logFile = open(logName, "a")
-        logFile.write(recepcion + "\n" + paquetesE + paquetesR + tiempoT + hashR + hash + separador)
-        logFile.close()
-
-
 host = ""
 BUFF = 1024
-port=20001
+port1=20001
+# TCP ------> socket.AF_INET, socket.SOCK_STREAM
+# UDP ------> socket.AF_INET, socket.SOCK_DGRAM
+s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-def servidor():
+# Bind to address and ip
+s.bind((host, port1))
+
+def servidor(dir):
     global numClientesC
     global atender
-    # TCP ------> socket.AF_INET, socket.SOCK_STREAM
-    # UDP ------> socket.AF_INET, socket.SOCK_DGRAM
-    s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-    # Bind to address and ip
-    s.bind((host, port))
-
+    numClientesC += 1
+    print("Numero Clientes Conectados: ", numClientesC)
+    sha1 = hashlib.sha1()
     while True:
-        numClientesC += 1
-        print("Numero Clientes Conectados: ", numClientesC)
-        sha1 = hashlib.sha1()
-        while True:
-            if (numClientesC >= numClientes or atender):
-                print("Starting to send")
-                break
-        atender = True
-        i = 0
-
-        s.send(fileT.encode())
-
-        time.sleep(0.01)
-
-        with open(fileName, 'rb') as f:
-            # print("Starting to send")
-            while True:
-                i += 1
-                data = f.read(BUFF)
-                if not data:
-                    break
-                sha1.update(data)
-
-                s.send(data)
-            print("Archivo Enviado")
-
-            # Envio de Hash
-            has = str(sha1.hexdigest())
-            s.send(("FINM" + has).encode())
-            f.close()
-
-            print('Fin envio')
-            numClientesC -= 1
-            print("Numero Clientes Conectados: ", numClientesC)
+        if (numClientesC >= numClientes or atender):
+            print("Starting to send")
             break
+    atender = True
+    i = 0
 
-for i in range(25):
-    t= threading.Thread(target=servidor, args=())
+    time.sleep(0.01)
+
+    with open(fileName, 'rb') as f:
+        # print("Starting to send")
+        while True:
+            i += 1
+            data = f.read(BUFF)
+            if not data:
+                break
+            sha1.update(data)
+
+            s.sendto(data, dir)
+        print("Archivo Enviado")
+
+        # Envio de Hash
+        has = str(sha1.hexdigest())
+        s.sendto(("FINM" + has).encode(), dir)
+        f.close()
+
+
+
+while True:
+    data = s.recvfrom(BUFF)
+    dir = data[1]
+    t = threading.Thread(target=servidor, args=(dir,))
     t.start()
-
